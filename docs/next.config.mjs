e13ecd276bca80ec50eeb5df4708d5e8f4c81531
Shomari <nyusternie@sdot.io>
2025-01-03 04:03:34 -0500

    Configure export.

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -6,21 +6,22 @@ import { remarkPlugins } from './src/mdx/remark.mjs'
 import withSearch from './src/mdx/search.mjs'
 
 const withMDX = nextMDX({
-  options: {
-    remarkPlugins,
-    rehypePlugins,
-    recmaPlugins,
-  },
+    options: {
+        remarkPlugins,
+        rehypePlugins,
+        recmaPlugins,
+    },
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    outputFileTracingIncludes: {
-      '/**/*': ['./src/app/**/*.mdx'],
+    output: 'export',
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+    experimental: {
+        outputFileTracingIncludes: {
+            '/**/*': ['./src/app/**/*.mdx'],
+        },
     },
-  },
 }
 
 export default withSearch(withMDX(nextConfig))
