a4e44e0ffb84acdc90c51b1a868f858c0913510c
Shomari <nyusternie@sdot.io>
2025-01-02 20:08:24 -0500

    Disable strict mode.

--- web/next.config.ts
+++ web/next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
     images: {
         unoptimized: true,
     },
+    // FIXME Temp fix to error
+    // "Application error: a client-side exception has occurred (see the browser console for more information)."
+    // (https://stackoverflow.com/questions/72607999/next-js-application-error-a-client-side-exception-has-occurred-see-the-browser)
+    reactStrictMode: false,
 }
 
 export default nextConfig
