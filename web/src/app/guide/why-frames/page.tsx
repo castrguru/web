ee65fca01d6b83be7c83ce2f36eb232f020554e1
Shomari <nyusternie@sdot.io>
2025-01-04 22:38:11 -0500

    Update app (path) config.

--- web/src/app/guide/why-frames/page.tsx
+++ web/src/app/guide/why-frames/page.tsx
@@ -24,22 +24,36 @@ export default function Example() {
                     <div className="pt-16 lg:row-span-2 lg:-mr-16 xl:mr-auto">
                         <div className="-mx-8 grid grid-cols-2 gap-4 sm:-mx-16 sm:grid-cols-4 lg:mx-0 lg:grid-cols-2 lg:gap-4 xl:gap-8">
                             <div className="aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10">
-                                <img alt="" src="https://images.unsplash.com/photo-1590650516494-0c8e4a4dd67e?&auto=format&fit=crop&crop=center&w=560&h=560&q=90" className="block size-full object-cover" />
+                                <Image
+                                    alt=""
+                                    width={0}
+                                    height={0}
+                                    src="https://images.unsplash.com/photo-1590650516494-0c8e4a4dd67e?&auto=format&fit=crop&crop=center&w=560&h=560&q=90"
+                                    className="block size-full object-cover"
+                                />
                             </div>
+
                             <div className="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10 lg:-mt-40">
                                 <img alt="" src="https://images.unsplash.com/photo-1557804506-669a67965ba0?&auto=format&fit=crop&crop=left&w=560&h=560&q=90" className="block size-full object-cover" />
                             </div>
+
                             <div className="aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10">
                                 <img alt="" src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?&auto=format&fit=crop&crop=left&w=560&h=560&q=90" className="block size-full object-cover" />
                             </div>
+
                             <div className="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10 lg:-mt-40">
                                 <img alt="" src="https://images.unsplash.com/photo-1598257006458-087169a1f08d?&auto=format&fit=crop&crop=center&w=560&h=560&q=90" className="block size-full object-cover" />
                             </div>
                         </div>
                     </div>
+
                     <div className="max-lg:mt-16 lg:col-span-1">
-                        <p className="text-base/7 font-semibold text-gray-500">The numbers</p>
+                        <p className="text-base/7 font-semibold text-gray-500">
+                            The numbers
+                        </p>
+
                         <hr className="mt-6 border-t border-gray-200" />
+
                         <dl className="mt-6 grid grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2">
                             <div className="flex flex-col gap-y-2 border-b border-dotted border-gray-200 pb-4">
                                 <dt className="text-sm/6 text-gray-600">Raised</dt>
--- web/src/components/HeaderAlt.tsx
+++ web/src/components/HeaderAlt.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { useState } from 'react'
-import { Dialog, DialogPanel, Popover, PopoverButton, PopoverGroup, PopoverPanel } from '@headlessui/react'
+import { Dialog, DialogPanel, PopoverGroup } from '@headlessui/react'
 import { ArrowPathIcon, Bars3Icon, ChartPieIcon, CursorArrowRaysIcon, FingerPrintIcon, SquaresPlusIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import { PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 import { BoltIcon } from '@heroicons/react/24/outline'
 
 import Image from 'next/image'
