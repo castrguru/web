6dabb835dc103e685e1adabae2dfda7c67739e3c
Shomari <nyusternie@sdot.io>
2025-01-04 22:04:40 -0500

    Add new frame kits.

--- web/src/app/kits/page.tsx
+++ web/src/app/kits/page.tsx
@@ -1,53 +1,126 @@
 import Image from 'next/image'
-import Link from 'next/link'
+// import Link from 'next/link'
 
-export default function Kits() {
+const kits = [
+    {
+        id: 1,
+        title: "Start Simple",
+        href: "#",
+        description: "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+        imageUrl: "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
+        date: "Mar 16, 2020",
+        datetime: "2020-03-16",
+        author: {
+            name: "Michael Foster",
+            imageUrl: "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+    },
+    {
+        id: 2,
+        title: "Start Minimal",
+        href: "#",
+        description: "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+        imageUrl: "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
+        date: "Mar 16, 2020",
+        datetime: "2020-03-16",
+        author: {
+            name: "Michael Foster",
+            imageUrl: "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+    },
+    {
+        id: 3,
+        title: "Avicii",
+        href: "#",
+        description: "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+        imageUrl: "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
+        date: "Mar 16, 2020",
+        datetime: "2020-03-16",
+        author: {
+            name: "Michael Foster",
+            imageUrl: "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+    },
+    {
+        id: 4,
+        title: "Britney",
+        href: "#",
+        description: "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+        imageUrl: "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
+        date: "Mar 16, 2020",
+        datetime: "2020-03-16",
+        author: {
+            name: "Michael Foster",
+            imageUrl: "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+    },
+    {
+        id: 5,
+        title: "Christina",
+        href: "#",
+        description: "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+        imageUrl: "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
+        date: "Mar 16, 2020",
+        datetime: "2020-03-16",
+        author: {
+            name: "Michael Foster",
+            imageUrl: "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+    },
+];
+
+export default function Example() {
     return (
-        <main className="p-5 flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            <h1 className="text-5xl font-light text-rose-400 tracking-widest italic">
-                Castr GÜRŲ Kits
-            </h1>
+        <div className="bg-white py-12 sm:py-16">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl text-center">
+                    <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                        Farcaster Frame Kits
+                    </h2>
 
-            <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                <li>
-                    <Link href="/demos/kitchen-sink" className="mb-2">
-                        Kitchen Sink Demos
-                    </Link>
-                </li>
+                    <p className="mt-2 text-lg/8 text-gray-600">
+                        Get started with these kits that give your ... The Kitchen Sink
+                    </p>
+                </div>
 
-                <li>
-                    <Link href="/demos/dashboard-01">
-                        Dashboard #1
-                    </Link>
-                </li>
-            </ol>
+                <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                    {kits.map((post) => (
+                        <article key={post.id} className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
+                            <Image
+                                alt=""
+                                width={0}
+                                height={0}
+                                src={post.imageUrl}
+                                className="absolute inset-0 -z-10 size-full object-cover"
+                            />
 
-            <div className="flex gap-4 items-center flex-col sm:flex-row">
-                <a
-                    className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        className="dark:invert"
-                        src="/vercel.svg"
-                        alt="Vercel logomark"
-                        width={20}
-                        height={20}
-                    />
-                    Get started
-                </a>
+                            <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
+                            <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
 
-                <a
-                    className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Read our docs
-                </a>
+                            <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm/6 text-gray-300">
+                                <time dateTime={post.datetime} className="mr-8">
+                                    {post.date}
+                                </time>
+                                <div className="-ml-4 flex items-center gap-x-4">
+                                    <svg viewBox="0 0 2 2" className="-ml-0.5 size-0.5 flex-none fill-white/50">
+                                        <circle r={1} cx={1} cy={1} />
+                                    </svg>
+                                    <div className="flex gap-x-2.5">
+                                        <img alt="" src={post.author.imageUrl} className="size-6 flex-none rounded-full bg-white/10" />
+                                        {post.author.name}
+                                    </div>
+                                </div>
+                            </div>
+                            <h3 className="mt-3 text-lg/6 font-semibold text-white">
+                                <a href={post.href}>
+                                    <span className="absolute inset-0" />
+                                    {post.title}
+                                </a>
+                            </h3>
+                        </article>
+                    ))}
+                </div>
             </div>
-        </main>
+        </div>
     )
 }
