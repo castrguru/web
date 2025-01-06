a59ccb182cf8604259cd629f6f5396cc30ce8494
Shomari <nyusternie@sdot.io>
2025-01-05 20:10:58 -0500

    Refactor manager layout.

--- web/src/components/FooterMin.tsx
+++ web/src/components/FooterMin.tsx
@@ -30,7 +30,7 @@ export default function FooterMin() { return (
         <div className="mx-auto max-w-7xl px-6 py-5 md:flex md:items-center md:justify-between lg:px-8">
             <div className="flex justify-center gap-x-6 md:order-2">
                 {navigation.map((item) => (
-                    <a key="{item.name}" href="{item.href}" className="text-gray-400 hover:text-gray-300">
+                    <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-300">
                         <span className="sr-only">{item.name}</span>
                         <item.icon aria-hidden={true} className="size-6" />
                     </a>
