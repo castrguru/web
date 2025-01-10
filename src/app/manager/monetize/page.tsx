e60bfa48de5722481f72f31c823256bf4daa2b14
Shomari <nyusternie@sdot.io>
2025-01-09 22:00:52 -0500

    Update license.

--- src/app/manager/monetize/page.tsx
+++ src/app/manager/monetize/page.tsx
@@ -7,15 +7,131 @@
 
 export default function Monetize() {
     return (
-        <div className="py-5 px-4 sm:px-6 lg:px-8 flex flex-col gap-4">
-            <h1 className="text-5xl font-medium text-amber-200 tracking-wider">
-                Monetize
-            </h1>
+        <div className="bg-white">
+            <main className="isolate">
+                {/* Content section */}
+                <div className="mt-16 overflow-hidden sm:mt-20">
+                    <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
+                        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
+                            <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
+                                <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                                    Our people
+                                </h2>
 
-            <p className="leading-8">
-                AD MANAGER
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, ab, in, iusto facere atque deserunt fuga minima deleniti earum nesciunt placeat ratione quos minus! Nam quod perferendis sequi ducimus repudiandae?
-            </p>
+                                <p className="mt-6 text-xl/8 text-gray-600">
+                                    Quasi est quaerat. Sit molestiae et.
+                                    Provident ad dolorem occaecati eos iste. Soluta rerum quidem minus ut molestiae velit error quod. Excepturi quidem expedita molestias quas.
+                                </p>
+
+                                <p className="mt-6 text-base/7 text-gray-600">
+                                    Anim aute id magna aliqua ad ad non deserunt sunt.
+                                    Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat. Quasi aperiam sit non sit neque reprehenderit.
+                                </p>
+                            </div>
+
+                            <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
+                                <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+                                    <img
+                                        alt=""
+                                        src="https://images.unsplash.com/photo-1670272502246-768d249768ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80"
+                                        className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                                    />
+                                </div>
+
+                                <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
+                                    <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
+                                        <img
+                                            alt=""
+                                            src="https://images.unsplash.com/photo-1605656816944-971cd5c1407f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
+                                            className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                                        />
+                                    </div>
+
+                                    <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
+                                        <img
+                                            alt=""
+                                            src="https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&h=842&q=80"
+                                            className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                                        />
+                                    </div>
+
+                                    <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
+                                        <img
+                                            alt=""
+                                            src="https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
+                                            className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Stats */}
+                <div className="mx-auto my-16 max-w-7xl px-6 sm:my-20 lg:px-8">
+                    <div className="mx-auto max-w-2xl lg:mx-0">
+                        <h2 className="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                            We approach work as a place to make the world better
+                        </h2>
+
+                        <p className="mt-6 text-base/7 text-gray-600">
+                            Diam nunc lacus lacus aliquam turpis enim. Eget hac velit est euismod lacus.
+                            Est non placerat nam arcu. Cras purus nibh cursus sit eu in id. Integer vel nibh.
+                        </p>
+                    </div>
+
+                    <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
+                        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-50 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
+                            <p className="flex-none text-3xl font-bold tracking-tight text-gray-900">
+                                250k
+                            </p>
+
+                            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                                <p className="text-lg font-semibold tracking-tight text-gray-900">
+                                    Users on the platform
+                                </p>
+
+                                <p className="mt-2 text-base/7 text-gray-600">
+                                    Vel labore deleniti veniam consequuntur sunt nobis.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
+                            <p className="flex-none text-3xl font-bold tracking-tight text-white">
+                                $8.9 billion
+                            </p>
+
+                            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                                <p className="text-lg font-semibold tracking-tight text-white">
+                                    We’re proud that our customers have made over $8 billion in total revenue.
+                                </p>
+
+                                <p className="mt-2 text-base/7 text-gray-400">
+                                    Eu duis porta aliquam ornare. Elementum eget magna egestas.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-indigo-600 p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
+                            <p className="flex-none text-3xl font-bold tracking-tight text-white">
+                                401,093
+                            </p>
+
+                            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                                <p className="text-lg font-semibold tracking-tight text-white">
+                                    Transactions this year
+                                </p>
+
+                                <p className="mt-2 text-base/7 text-indigo-200">
+                                    Eu duis porta aliquam ornare. Elementum eget magna egestas. Eu duis porta aliquam ornare.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
