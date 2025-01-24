161574a5b8e4c5efe0f42bd06a7029d744bf630a
Shomari <nyusternie@sdot.io>
2025-01-23 23:25:26 -0500

    Update next page.

--- src/app/lib/astro/page.tsx
+++ src/app/lib/astro/page.tsx
@@ -97,7 +97,7 @@ function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(" ");
 }
 
-export default function Example() {
+export default function LibAstro() {
     return (
         <div className="bg-white">
             <div className="mx-auto px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
