4f7a9f057e7930b18cbcbb7da41899ee60482c57
Shomari <nyusternie@sdot.io>
2025-01-25 12:18:57 -0500

    Update title.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const FRAME_METADATA = {
     // imageUrl: 'https://castr.guru/og.png',
     imageUrl: 'https://castr.guru/banner.jpg?' + moment().unix(),
     button: {
-        title: `👨‍💻 🤖 Build Your Imagination 💡 🚀`,
+        title: `👨‍💻 🤖 Come Build Ur Dreams 💡 🚀`,
         action: {
             type: 'launch_frame',
             name: 'Launcher',
